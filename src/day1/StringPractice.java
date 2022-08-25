30def32ad2f00937633fcf318b596f563747f1ec
parhatmamat <parhatborani@gmail.com>
2022-08-24 23:35:12 -0700

    third change

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -7,5 +7,6 @@ public class StringPractice
     public static void main(String[] args) {
         System.out.printf("Hello World");
         System.out.println("Java is fun");
+        System.out.println("Java is fun");
     }
 }
