62ddea289d4a6f73fe2d04f06729673b109f16de
parhatmamat <parhatborani@gmail.com>
2022-08-24 23:33:03 -0700

    String methos is changed

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -1,8 +1,11 @@
 package day1;
 
+import java.sql.SQLOutput;
+
 public class StringPractice
 {
     public static void main(String[] args) {
         System.out.printf("Hello World");
+        System.out.println("Java is fun");
     }
 }
